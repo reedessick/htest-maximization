6ac8b0d73ac772e00fbe7444ad4eda4ffdb26582
Reed Essick <reed.essick@gmail.com>
2021-09-30 21:19:33 -0500

    more work on objects

--- poisson/utils.py
+++ poisson/utils.py
@@ -6,37 +6,47 @@ __author__ = "Reed Essick (reed.essick@gmail.com)"
 
 import numpy as np
 
+from scipy.special import gammaln
+
+#-------------------------------------------------
+
+def logfactorial(x):
+    """natural logarithm of the factorial
+    """
+    return gammaln(x+1)
+
 #-------------------------------------------------
 
 class XrayProcess(object):
     """a model for generating realizations of periodic Poissonian X-ray events
     """
 
-    def __init__(self, mean, *fourier_components):
+    def __init__(self, mean, *ac_components):
         self.mean = mean
-        self.fourier_components = []
+        self.ac_components = list(ac_components)
+        self._check() ### check that the differential rate is always positive
+
+    def _check(self):
         raise NotImplementedError('''\
-if fourier_components is empty, set this up as independent of phase (m=0 harmonic only)
-store the fourier components
-normalize them so they produce the overall mean
 sanity check the signal model to make sure the differential rate is always non-negative
+do this via brute-force by generating sampling that is much finer than the highest harmonic present and directly checking the model everywhere?
 ''')
+
+    def binned_mean(self, low, high):
+        """return the expected number of events in phases \in [low, high]
+        """
+        raise NotImplementedError('integrate this analytically. Or numerically using our "really fine" grid?')
 
     def rvs(size=1):
         """draw random variates from this model
         """
-        raise NotImplementedError
+        raise NotImplementedError('draw samples from this analytically? Or compute CDF with finer spacing than the highest harmonic and then simply inverse-transform sample?')
 
     def __add__(self, other):
         """return a new instance that combines the Poissonian processes
         """
         assert isinstance(other, XrayProcess), 'can only add XrayProcess to another XrayProcess'
-        raise NotImplementedError
-
-    def binned_mean(self, low, high):
-        """return the expected number of events in phases \in [low, high]
-        """
-        raise NotImplementedError
+        return XrayProcess(self.mean+other.mean, *(self.ac_components+other.ac_components))
 
 #------------------------
 
@@ -49,12 +59,36 @@ class Likelihood(object):
         if isinstance(bins, int): ### generate this number of equally spaced bins
             bins = np.linspace(0, 2*np.pi, bins+1)
         self.bins = bins
+        self._check() ### sanity check the bins
+
+    def _check(self):
+        edge = 0.0
+        for i in range(len(self.bins)-1):
+            assert self.bins[i]==edge, 'bin %d does not start where previous bin ended!'
+            edge = self.bins[i+1] ### move edge to the end of this bin
+        assert edge==2*np.pi, 'bins do not end at 2*pi'
+
+    @property
+    def nbins(self):
+        return len(self.bins)
+
+    @property
+    def mean(self):
+        return self.process.mean
+
+    @property
+    def binned_means(self):
+        return np.array([self.process.binned_mean(bins[i], bins[i+1]) for i in range(self.nbins)])
 
     def bin(self, data):
-        raise NotImplementedError('map the data into counts per bin')
+        """map data (an array of phases) into counts in each bin
+        """
+        return np.histogram(data, self.bins)[0] ### only return the array of counts
 
     def prob(self, binned_data):
-        raise NotImplementedError('compute the probability over all bins together')
+        return np.exp(self.logprob(binned_data))
 
     def logprob(self, binned_data):
-        raise NotImplementedError('compute the natural log of the probability over all bins together')
+        """the natural log of the probability of observing binned_data given our binning and our process
+        """
+        return -self.mean + np.sum(binned_data*np.log(self.binned_means) - logfactorial(binned_data))
